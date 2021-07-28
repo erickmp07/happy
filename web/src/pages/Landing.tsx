cf58efdcfab40b9ccb8f949af06d8f4396954c0f
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-07-28 18:09:28 -0300

    Set navigation to SPA

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,4 +1,5 @@
 import { FiArrowRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 import "../styles/pages/landing.css";
 
@@ -20,9 +21,9 @@ function Landing() {
                     <span>Rio de Janeiro</span>
                 </div>
 
-                <a href="" className="enter-app">
+                <Link to="/app" className="enter-app">
                     <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-                </a>
+                </Link>
             </div>
         </div>
     );
