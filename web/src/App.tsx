007efcaaef6be32f6334d9ace04987c439cef2ab
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-07-27 23:31:25 -0300

    Add elements to the landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,28 @@
 import "./styles/global.css";
 import "./styles/pages/landing.css";
 
+import logoImg from "./assets/images/logo.svg";
+
 function App() {
     return (
         <div id="page-landing">
+            <div className="content-wrapper">
+                <img src={logoImg} alt="Happy" />
 
+                <main>
+                    <h1>Bring happiness to the world</h1>
+                    <p>Visit residential institutions for children and change their day.</p>
+                </main>
+
+                <div className="location">
+                    <strong>Rio de Janeiro</strong>
+                    <span>Rio de Janeiro</span>
+                </div>
+
+                <a href="" className="enter-app">
+                    &gt;
+                </a>
+            </div>
         </div>
     );
 }
