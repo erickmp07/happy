11ce0137efbbc75ec1ce1bcfb6425783f8d3a947
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-08-12 19:21:35 -0300

    Create Institutions controller

---
+++ backend/src/controllers/InstitutionsController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Institution from "../models/Institutions";
+
+export default {
+    async create(request: Request, response: Response) {
+        const {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        } = request.body;
+    
+        const institutionsRepository = getRepository(Institution);
+    
+        const institution = institutionsRepository.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        });
+    
+        await institutionsRepository.save(institution);
+    
+        return response.status(201).json(institution);
+    }
+};
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,35 +1,8 @@
 import { Router, Request, Response } from "express";
-import { getRepository } from "typeorm";
-import Institution from "./models/Institutions";
+import InstitutionsController from "./controllers/InstitutionsController";
 
 const routes = Router();
 
-routes.post("/institutions", async (request: Request, response: Response) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body;
-
-    const institutionsRepository = getRepository(Institution);
-
-    const institution = institutionsRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-
-    await institutionsRepository.save(institution);
-
-    return response.status(201).json(institution);
-});
+routes.post("/institutions", InstitutionsController.create);
 
 export default routes;
