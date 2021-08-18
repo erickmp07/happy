4916491182db389ebf08d2c47a52338cd3377915
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-08-17 23:55:44 -0300

    Add routes to tha pages to create and list detail

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import InstitutionsMap from "./pages/InstitutionsMap";
+import Institution from "./pages/Institution";
+import CreateInstitution from "./pages/CreateInstitution";
 
 function Routes() {
     return (
@@ -9,6 +11,9 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={InstitutionsMap} />
+                
+                <Route path="/institutions/create" component={CreateInstitution} />
+                <Route path="/institutions/:id" component={Institution} />
             </Switch>
         </BrowserRouter>
     );
