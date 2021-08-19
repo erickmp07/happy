ba4ba414f52ea1a068b7df40175a3b2ee34c0e02
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-08-19 20:46:12 -0300

    Create mobile project

---
+++ mobile/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import  MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <MapView 
+                provider={PROVIDER_GOOGLE}
+                style={styles.map} 
+                initialRegion={{
+                    latitude: -22.835987,
+                    longitude: -43.306520,
+                    latitudeDelta: 0.008,
+                    longitudeDelta: 0.008
+                }} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    map: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height
+    }
+});
