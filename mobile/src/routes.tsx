8613b758f5f01ee4ef18bd4dfe25c13bb2f144df
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-08-20 00:46:21 -0300

    Add route to Institution details page

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -6,12 +6,15 @@ import { createStackNavigator } from "@react-navigation/stack";
 const { Navigator, Screen } = createStackNavigator();
 
 import InstitutionsMap from "./pages/InstitutionsMap";
+import InstitutionDetails from "./pages/InstitutionDetails";
 
 export default function Routes() {
     return (
         <NavigationContainer>
             <Navigator screenOptions={{ headerShown: false }}>
                 <Screen name="InstitutionsMap" component={InstitutionsMap} />
+
+                <Screen name="InstitutionDetails" component={InstitutionDetails} />
             </Navigator>
         </NavigationContainer>
     );
