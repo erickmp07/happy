0429aea3cef068da3783f1024db608268b8b21d9
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-08-19 23:50:29 -0300

    Create footer element

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import  MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { Feather } from "@expo/vector-icons";
 
 import mapMarker from "./src/images/map-marker.png";
 
@@ -35,6 +36,14 @@ export default function App() {
                     </Callout>
                 </Marker>
             </MapView>
+
+            <View style={styles.footer}>
+                <Text style={styles.footerText}>2 institutions found</Text>
+
+                <TouchableOpacity style={styles.createInstitutionButton} onPress={() => {}}>
+                    <Feather name="plus" size={20} color="#fff" />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -61,5 +70,35 @@ const styles = StyleSheet.create({
     calloutText: {
         color: "#0089a5",
         fontSize: 14
+    },
+
+    footer: {
+        position: "absolute",
+        left: 24,
+        right: 24,
+        bottom: 32,
+
+        backgroundColor: "#fff",
+        borderRadius: 20,
+        height: 56,
+        paddingLeft: 24,
+
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+
+    footerText: {
+        color: "#8fa7b3"
+    },
+
+    createInstitutionButton: {
+        width: 56,
+        height: 56,
+        backgroundColor: "#15c3d6",
+        borderRadius: 20,
+
+        justifyContent: "center",
+        alignItems: "center"
     }
 });
