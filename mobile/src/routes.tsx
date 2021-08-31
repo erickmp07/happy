7137b7316c7449a18241103f66c97b5b62dd3e92
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-08-30 23:12:30 -0300

    Create routes to the pages to create Institution

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -8,6 +8,9 @@ const { Navigator, Screen } = createStackNavigator();
 import InstitutionsMap from "./pages/InstitutionsMap";
 import InstitutionDetails from "./pages/InstitutionDetails";
 
+import SelectMapPosition from "./pages/CreateInstitution/SelectMapPosition";
+import InstitutionData from "./pages/CreateInstitution/InstitutionData";
+
 export default function Routes() {
     return (
         <NavigationContainer>
@@ -15,6 +18,10 @@ export default function Routes() {
                 <Screen name="InstitutionsMap" component={InstitutionsMap} />
 
                 <Screen name="InstitutionDetails" component={InstitutionDetails} />
+
+                <Screen name="SelectMapPosition" component={SelectMapPosition} />
+
+                <Screen name="InstitutionData" component={InstitutionData} />
             </Navigator>
         </NavigationContainer>
     );
