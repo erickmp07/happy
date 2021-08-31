d4cca26127b857f63ee0bee4ddf145cff2978d65
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-08-31 00:08:49 -0300

    Add the Header component to some pages

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -11,17 +11,43 @@ import InstitutionDetails from "./pages/InstitutionDetails";
 import SelectMapPosition from "./pages/CreateInstitution/SelectMapPosition";
 import InstitutionData from "./pages/CreateInstitution/InstitutionData";
 
+import Header from "./components/Header";
+
 export default function Routes() {
     return (
         <NavigationContainer>
             <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: "#f2f3f5" } }}>
-                <Screen name="InstitutionsMap" component={InstitutionsMap} />
+                <Screen 
+                    name="InstitutionsMap" 
+                    component={InstitutionsMap} 
+                />
 
-                <Screen name="InstitutionDetails" component={InstitutionDetails} />
+                <Screen 
+                    name="InstitutionDetails" 
+                    component={InstitutionDetails}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title="Institution" />
+                    }}
+                />
 
-                <Screen name="SelectMapPosition" component={SelectMapPosition} />
+                <Screen 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Select on the map" />
+                    }} 
+                />
 
-                <Screen name="InstitutionData" component={InstitutionData} />
+                <Screen 
+                    name="InstitutionData" 
+                    component={InstitutionData} 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Add the Institution" />
+                    }}
+                />
             </Navigator>
         </NavigationContainer>
     );
